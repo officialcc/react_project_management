31ad74e754ad803deea6ee7deeb6c7a6957ea0a5
colin <163393924+officialcc@users.noreply.github.com>
2025-12-30 16:53:37 +0800

    Completed Lesson 163

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({ onAdd, onCancel }) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -12,6 +15,15 @@ export default function NewProject({onAdd}) {
         const enteredDueDate = dueDate.current.value;
 
         // TODO: Validation
+        if (
+            enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim() === ''
+        ) {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -20,9 +32,19 @@ export default function NewProject({onAdd}) {
     }
 
     return (
+        <>
+        <Modal ref={modal} buttonCaption="Okay">
+            <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+            <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+            <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+        </Modal>
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+                <li>
+                    <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
+                        Cancel
+                    </button>
+                </li>
                 <li>
                     <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>
                         Save
@@ -35,5 +57,6 @@ export default function NewProject({onAdd}) {
                 <Input type="date" ref={dueDate} label="Due Date" />
             </div>
         </div>
+        </>
     )
 }
